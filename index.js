e5958544760465ee53516dc3470533e99dc363d7
Jovianto Trisila <mandevil41@gmail.com>
2022-05-28 21:51:57 +0700

    Rev color & favResto

--- index.js
+++ index.js
@@ -2,25 +2,27 @@
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
 
-const setColorMonica = new Set();
-setColorMonica.add('Yellow');
-setColorMonica.add('Pink');
-setColorMonica.add('White');
-setColorMonica.add('Purple');
-setColorMonica.add('White');
+const setColorMonica = new Set(['Yellow', 'Pink', 'White', 'Purple']);
+// const setColorMonica = new Set();
+// setColorMonica.add('Yellow');
+// setColorMonica.add('Pink');
+// setColorMonica.add('White');
+// setColorMonica.add('Purple');
+// setColorMonica.add('White');
 
-const setRestoMonica = new Set ();
-setRestoMonica.add ('Bento');
-setRestoMonica.add ('Sushi');
-setRestoMonica.add ('Pancake');
-setRestoMonica.add ('Eggy');
-setRestoMonica.add ('Tempura');
-setRestoMonica.add ('Bento');
-setRestoMonica.add ('Eggy');
-setRestoMonica.add ('Padang');
-setRestoMonica.add ('Tteok');
-setRestoMonica.add ('Sushi');
-setRestoMonica.add ('Sushi');
+const setRestoMonica = new Set (['Bento','Sushi','Pancake','Eggy','Tempura','Bento','Eggy','Padang','Tteok','Sushi','Sushi']);
+// const setRestoMonica = new Set ();
+// setRestoMonica.add ('Bento');
+// setRestoMonica.add ('Sushi');
+// setRestoMonica.add ('Pancake');
+// setRestoMonica.add ('Eggy');
+// setRestoMonica.add ('Tempura');
+// setRestoMonica.add ('Bento');
+// setRestoMonica.add ('Eggy');
+// setRestoMonica.add ('Padang');
+// setRestoMonica.add ('Tteok');
+// setRestoMonica.add ('Sushi');
+// setRestoMonica.add ('Sushi');
 
 
 const firstUser = {
@@ -28,43 +30,45 @@ const firstUser = {
     gender: 'Female',
     age: 17,
     email:'monica@dingdong.com',
-    favoriteColor: setColorMonica,
+    favoriteColor: Array.from(setColorMonica),
     isHavePet: 'Yes',
     education: [{name: 'SD 01', city: 'Jakarta', graduate: 2016}, 
        {name: 'SMP 02', city: 'Jakarta', graduate: 2019},
        {name: 'SMA 03', city: 'Tangerang'}],
-    favoriteRestaurant: setRestoMonica
+    favoriteRestaurant: Array.from(setRestoMonica)
 };
 
-const setColorWendy = new Set();
-setColorWendy.add ('Blue');
-setColorWendy.add ('Black');
-setColorWendy.add ('Grey');
-setColorWendy.add ('Grey');
+const setColorWendy = new Set(['Blue', 'Black', 'Grey']);
+// setColorWendy = new Set();
+// setColorWendy.add ('Blue');
+// setColorWendy.add ('Black');
+// setColorWendy.add ('Grey');
+// setColorWendy.add ('Grey');
 
-const setRestoWendy = new Set ();
-setRestoWendy.add ('Tempura');
-setRestoWendy.add ('Bento');
-setRestoWendy.add ('Sushi');
-setRestoWendy.add ('Pancake');
-setRestoWendy.add ('Padang');
-setRestoWendy.add ('Katsu');
-setRestoWendy.add ('Geprek');
-setRestoWendy.add ('Pancake');
-setRestoWendy.add ('Eggy');
+const setRestoWendy = new Set (['Tempura', 'Bento', 'Sushi', 'Pancake', 'Padang', 'Katsu', 'Geprek', 'Pancake', 'Eggy']);
+// setRestoWendy = new Set ();
+// setRestoWendy.add ('Tempura');
+// setRestoWendy.add ('Bento');
+// setRestoWendy.add ('Sushi');
+// setRestoWendy.add ('Pancake');
+// setRestoWendy.add ('Padang');
+// setRestoWendy.add ('Katsu');
+// setRestoWendy.add ('Geprek');
+// setRestoWendy.add ('Pancake');
+// setRestoWendy.add ('Eggy');
 
 const secondUser = {
     name: 'Wendy',
     gender: 'Male',
     age: 23,
     email: 'wendy@dingdong.com',
-    favoriteColor: setColorWendy,
+    favoriteColor: Array.from(setColorWendy),
     education: [{name: 'SD 02', city: 'Jakarta', graduate: 2010}, 
     {name: 'SMP 03', city: 'Bogor', graduate: 2013},
     {name: 'SMA 01', city: 'Surabaya', graduate: 2016}, 
     {name: 'Universitas Maju', city:'Tangerang'}],
     isHavePet: 'No',
-    favoriteRestaurant: setRestoWendy
+    favoriteRestaurant: Array.from(setRestoWendy)
 };
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
